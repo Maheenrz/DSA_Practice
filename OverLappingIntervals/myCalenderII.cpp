17dc9f4907e378b7b7725de988d5832d17c00ab0
Maheenrz <mahheen508@gmail.com>
2025-07-04 23:07:16 +0500

    Solution

--- OverLappingIntervals/myCalenderII.cpp
+++ OverLappingIntervals/myCalenderII.cpp
@@ -1,11 +1,33 @@
 class MyCalendarTwo {
+private:
+    vector<pair<int, int>> all_bookings;
+    vector<pair<int, int>> overlapped;
+
 public:
-    MyCalendarTwo() {
-        
-    }
-    
+    MyCalendarTwo() {}
+
     bool book(int startTime, int endTime) {
-        
+        if (all_bookings.empty()) {
+            all_bookings.push_back({startTime, endTime});
+        }
+
+        if (all_bookings.size() == 1) {
+            if (all_bookings[0].second >= startTime) {
+                overlapped.push_back({startTime, endTime});
+                overlapped.push_back(all_bookings[0]);
+            }
+        } else {
+            sort(overlapped.begin(), overlapped.end());
+
+            for (const auto& over_lap : overlapped) {
+                if (over_lap.second >= startTime) {
+                    return false;
+                }
+            }
+        }
+
+        all_bookings.push_back({startTime, endTime});
+        return true;
     }
 };
 
